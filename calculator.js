474f390ac9076eb2be480047470e355b6fc74c6d
PrishaAnand04 <132134007+PrishaAnand04@users.noreply.github.com>
2023-12-25 16:50:49 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,47 @@
+// var a=prompt("enter a number ")
+// var b=prompt("enter another number")
+// var c=prompt("enter the number denoted to a function: 1-->sum,2-->subtraction,3-->multiplication,4-->division,5-->Mod")
+
+// if(c==1){   
+//     var result= a + b
+// }
+
+// else if(c==2){
+//     var result=a-b
+// }
+
+// else if(c==3){
+//     var result=a*b
+// }
+// else if(c==4){
+//     var result=a/b
+// }
+// else if(c==5){
+//     var result=a%b
+// }
+// else{
+//     console.log("inccorect input for function")
+// }
+// console.log("result is " + result)
+
+let random= Math.random()
+let a=prompt("enter first number ")
+let c=prompt("enter operation ")
+let b=prompt("enter second number ")
+
+
+let obj={
+    "+":"-",
+    "*":+"+",
+    "-":"/",
+    "/":"**",
+}
+
+if (random>0.1){
+    
+    // alert("the result is" + )
+}
+else{
+    c=obj[c]
+    // alert("the result is"+)
+}
